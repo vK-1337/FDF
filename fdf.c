664247936c857e4f71319e2248ab5be5961b6352
Vincent Da Conceicao Ramos <vda-conc@paul-f3Br6s6.clusters.42paris.fr>
2024-01-08 20:36:50 +0100

    great

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: vda-conc <vda-conc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 10:47:59 by vda-conc          #+#    #+#             */
-/*   Updated: 2024/01/07 15:39:17 by vda-conc         ###   ########.fr       */
+/*   Updated: 2024/01/08 19:51:07 by vda-conc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	main(int ac, char **av)
 {
 	int		fd;
 	char	***map;
+  t_point ***struct_map;
 
 	if (ac < 2)
 		return (0);
@@ -25,7 +26,9 @@ int	main(int ac, char **av)
 	map = ft_create_map_from_file(fd);
 	if (!map)
 		return (close(fd), 0);
-	ft_fdf(map);
+  struct_map = ft_convert_map(map);
+  ft_print_struct_map(struct_map);
+	ft_fdf(struct_map);
 	return (0);
 }
 
@@ -33,11 +36,13 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
+  if ( x > WIDTH || y > HEIGHT || x < 0 || y < 0)
+    return;
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
 }
 
-int	ft_fdf(char ***map)
+int	ft_fdf(t_point ***map)
 {
 	void	*mlx;
 	void	*mlx_win;
@@ -48,13 +53,63 @@ int	ft_fdf(char ***map)
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 			&img.endian);
+  ft_cast_whole_map(map, 45);
 	draw_line(map, &img);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 	return (0);
 }
 
-void	draw_line(char ***map, t_data *img)
+void ft_print_struct_map(t_point ***struct_map)
+{
+  int i = 0, j;
+  while (struct_map[i])
+  {
+    j = 0;
+    while (struct_map[i][j])
+    {
+      printf("x = %f, y = %f, z = %f, color = %d\n", struct_map[i][j]->x, struct_map[i][j]->y, struct_map[i][j]->z, struct_map[i][j]->color);
+      j++;
+    }
+    i++;
+  }
+}
+
+void ft_rotate_z(t_point *point, int angle)
+{
+  point->x = point->x * cos(angle) - point->y * sin(angle);
+  point->y = point->x * sin(angle) + point->y * cos(angle);
+}
+
+void ft_rotate_x(t_point *point, int angle)
+{
+  point->y = point->x * cos(angle) - point->y * sin(angle);
+}
+void ft_cast_point(t_point *point, int angle)
+{
+  ft_rotate_z(point, angle);
+  ft_rotate_x(point, angle);
+}
+
+void ft_cast_whole_map(t_point ***map, int angle)
+{
+  int i;
+  int j;
+
+  i = 0;
+  while (map[i])
+  {
+    j = 0;
+    while (map[i][j])
+    {
+      ft_cast_point(map[i][j], angle);
+      j++;
+    }
+    i++;
+  }
+}
+
+void	draw_line(t_point ***map, t_data *img)
 {
 	int	i;
 	int	j;
@@ -63,34 +118,56 @@ void	draw_line(char ***map, t_data *img)
   double y1;
   double x2;
   double y2;
-  int x3;
-  int y3;
-  int x4;
-  int y4;
+  double x3;
+  double y3;
+  double x1_tan;
+  double y1_tan;
+  double x2_tan;
+  double y2_tan;
+  double x3_tan;
+  double y3_tan;
 	int	y;
 	int	space;
 
 	i = 0;
 	space = 50;
-	y = 300;
+	y = -500;
 	while (map[i])
 	{
 		x = 800;
 		j = 0;
 		while (map[i][j])
 		{
-      x1 = (int)ft_first_rot_x(x, y);
-      y1 = (int)ft_first_rot_y(x, y);
-      x2 = (int)ft_first_rot_x(x + space, y);
-      y2 = (int)ft_first_rot_y(x, y + space);
-      x3 = (int)ft_arc_tan_x(x1);
-      y3 = (int)ft_arc_tan_y(x2, y1, ft_atoi(map[i][j]));
-      x4 = (int)ft_arc_tan_x(x2);
-      y4 = (int)ft_arc_tan_y(x1, y2, ft_atoi(map[i][j]));
-			if ((map[i + 1] && map[i + 1][j]) || i == 0)
-				bresenham(x3, y3, x4, y3, img);
-			if (map[i][j + 1])
-				bresenham(x3, y3, x3, y4, img);
+      if (j < 17 && i < 10)
+      {
+        x1 = ft_first_rot_x(x, y);
+        y1 = ft_first_rot_y(x, y);
+        //
+        x2 = ft_first_rot_x(x + space, y);
+        y2 = ft_first_rot_y(x + space, y);
+        //
+        x3 = ft_first_rot_x(x, y + space);
+        y3 = ft_first_rot_y(x, y + space);
+        // //
+        x1_tan = ft_cast_point(map[i][j], 45);
+        y1_tan = ft_arc_tan_y(x1, y1, ft_atoi(map[i][j]));
+        //
+        x2_tan = ft_arc_tan_x(x2);
+        if (j == 19)
+          y2_tan = ft_arc_tan_y(x2, y2, ft_atoi(map[i][j]));
+        else
+          y2_tan = ft_arc_tan_y(x2, y2, ft_atoi(map[i][j + 1]));
+        x3_tan = ft_arc_tan_x(x3);
+        if (i == 18)
+          y3_tan = ft_arc_tan_y(x3, y3, ft_atoi(map[i][j]));
+        else
+          y3_tan = ft_arc_tan_y(x3, y3, ft_atoi(map[i + 1][j]));
+        //
+      }
+			if ((j < ft_linelen(map[i])) && (map[i][j + 1]))
+				bresenham(round(x1_tan), round(y1_tan), round(x2_tan), round(y2_tan), img);
+			if ((i < ft_maplen(map) - 1) && map[i + 1][j])
+				bresenham(round(x1_tan), round(y1_tan), round(x3_tan), round(y3_tan), img);
 			x += space;
 			j++;
 		}
@@ -98,6 +175,11 @@ void	draw_line(char ***map, t_data *img)
 		i++;
 	}
 }
+
+// if ((j < ft_linelen(map[i])) && (map[i][j + 1]))
+// 				bresenham(round(x1_tan), round(y1_tan), round(x2_tan), round(y2_tan), img);
+// if ((i < ft_maplen(map) - 1) && map[i + 1][j])
+//   bresenham(round(x1_tan), round(y1_tan), round(x3_tan), round(y3_tan), img);
 
 void	bresenham(int x1, int y1, int x2, int y2, t_data *img)
 {
@@ -133,7 +215,7 @@ void	bresenham(int x1, int y1, int x2, int y2, t_data *img)
 double ft_first_rot_x(int x, int y)
 {
   double x1;
-  x1 = x * (sqrt(2) / 2) - y * (sqrt(2) / 2);
+  x1 = (x * (sqrt(2) / 2)) - (y * (sqrt(2) / 2));
   printf("x1 = %f\n", x1);
   return (x1);
 }
@@ -141,7 +223,7 @@ double ft_first_rot_x(int x, int y)
 double ft_first_rot_y(int x, int y)
 {
   double y1;
-  y1 = x * (sqrt(2) / 2) + y * (sqrt(2) / 2);
+  y1 = (x * (sqrt(2) / 2)) + (y * (sqrt(2) / 2));
   printf("y1 = %f\n", y1);
   return (y1);
 }
@@ -156,6 +238,8 @@ double ft_arc_tan_x(double x)
 double ft_arc_tan_y(double x, double y, int z)
 {
   double y1;
-  y1 = x * 0 + ( y * sqrt(3)/3) - (z * sqrt(6)/3);
+  (void)x;
+  printf("Z => |%d|\n", z);
+  y1 = (y * (sqrt(3)/3)) - (z * (sqrt(6)/3));
   return (y1);
 }
--- fdf.h
+++ fdf.h
@@ -6,13 +6,17 @@
 # include "./minilibx-linux/mlx.h"
 # include <fcntl.h>
 # include <math.h>
+# include <math.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/stat.h>
 # include <sys/types.h>
-#include <math.h>
-// STRUCT //
 
+# define WIDTH 1920
+# define HEIGHT 1080
+
+
+// STRUCT //
 typedef struct s_data
 {
 	void	*img;
@@ -22,41 +26,53 @@ typedef struct s_data
 	int		endian;
 }			t_data;
 
+typedef struct s_point
+{
+	double		x;
+	double		y;
+	double		z;
+	int		color;
+}			t_point;
+
 // MAIN FUNCTION //
-int			ft_fdf(char ***map);
+int			ft_fdf(t_point ***map);
 
 // PARSING //
 int			ft_maplen(char ***map);
-char	***ft_join_map_lines(char ***map, char **new_line);
+char		***ft_join_map_lines(char ***map, char **new_line);
 char		***ft_create_map_from_file(int fd);
 int			ft_valid_file(char *file_name);
 char		*remove_spaces_and_newlines(char *str);
-char ***copy_map_content(char ***map, char ***new_map, char **new_line, int maplen);
+char		***copy_map_content(char ***map, char ***new_map, char **new_line,
+				int maplen);
 int			get_map_length(char ***map);
-int get_line_length(char **new_line);
-void ft_trim_nl(char **points);
-char ***ft_add_map_line(char ***map, char **newline);
-char ***ft_join_map(char ***old_map, char** new_line);
-char **ft_null_line(int line_len);
-int ft_linelen(char **line);
-void print_map(char ***map);
+int			get_line_length(char **new_line);
+void		ft_trim_nl(char **points);
+char		***ft_add_map_line(char ***map, char **newline);
+char		***ft_join_map(char ***old_map, char **new_line);
+char		**ft_null_line(int line_len);
+int			ft_linelen(char **line);
+void		print_map(char ***map);
+t_point ***ft_convert_map(char ***map);
+void ft_print_struct_map(t_point ***struct_map);
+t_point *ft_convert_point(int x, int y, char *z);
 // MEMORY //
 void		ft_free_memory(char **tab);
 void		free_old_map(char ***map, int maplen);
-char    ***allocate_new_map(int maplen, int line_length);
+char		***allocate_new_map(int maplen, int line_length);
 // DEBUG //
 void		ft_print_all_lines(char **lines_tab);
 
 // CALCULATIONS //
-double ft_first_rot_x(int x, int y);
-double ft_first_rot_y(int x, int y);
-double ft_arc_tan_x(double x);
-double ft_arc_tan_y(double x, double y, int z);
+double		ft_first_rot_x(int x, int y);
+double		ft_first_rot_y(int x, int y);
+double		ft_arc_tan_x(double x);
+double		ft_arc_tan_y(double x, double y, int z);
 
 // GRAPHICS //
 void		draw_grid_from_matrix(t_data *data, char ***map, int grid_spacing,
 				int color);
 void		bresenham(int x1, int y1, int x2, int y2, t_data *img);
-void		draw_line(char ***map, t_data *img);
+void		draw_line(t_point ***map, t_data *img);
 
 #endif
